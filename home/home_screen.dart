56d85a661f499fa93e06348e900163b7bb0ca33a
Sina <sinaelahu@gmail.com>
2026-05-08 22:14:29 +0330

    homescreen Added in main file

---
+++ home/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sabri/timer/timerscreen.dart';
+
+class Homescreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Text("Hosgeldin sinanin programina"),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Timerscreen()));
+                },
+                child: Text("BURAYA BASMA")
+            )
+
+          ],
+        )
+      )
+    );
+  }
+
+}
